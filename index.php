11607ee08fe274c3e2c82aeb0a1e8865ada611e1
Rama Novaris <ramaanovariss@gmail.com>
2023-06-19 00:33:07 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, natus atque quod et, neque velit iure quis, sequi expedita blanditiis laborum deserunt accusamus? Architecto earum soluta repellendus alias labore numquam.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 
 </html>
